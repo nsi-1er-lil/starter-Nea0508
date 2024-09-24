ec4e365d1b163c469d52732b3fef33588418c1b9
Néa Gubert <neag@lyceechurchill.london>
2024-09-24 14:22:39 +0000

    commit c

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -5,13 +5,13 @@ A = 4**2
 b = A>5
 
 # Question 7:
-def perimetre(x):
+def perimetre(c):
     return(4*c)
 
 print(perimetre(5))
 
 # Question 8:
-def surface(x):
-    return(4**2)
+def surface(c):
+    return(c**2)
 
 print(surface(5))

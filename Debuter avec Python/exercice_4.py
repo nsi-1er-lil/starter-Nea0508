1d8479cb9c15e0709bc992b375fbd6aa7aa593ac
Néa Gubert <neag@lyceechurchill.london>
2024-09-24 14:07:53 +0000

    commit

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,10 +1,17 @@
 # Question 6:
 c = 5
 P = 4*c
-A = 4^2
+A = 4**2
 b = A>5
 
 # Question 7:
 def perimetre(x):
-    return 4*c
+    return(4*c)
 
+print(perimetre(5))
+
+# Question 8:
+def surface(x):
+    return(4**2)
+
+print(surface(5))

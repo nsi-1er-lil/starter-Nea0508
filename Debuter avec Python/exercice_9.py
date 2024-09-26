e377517f22ff6fd90fdc4a57a96c5e3511eeaa06
Néa Gubert <neag@lyceechurchill.london>
2024-09-26 12:07:04 +0000

    exo9

---
+++ Debuter avec Python/exercice_9.py
@@ -0,0 +1,6 @@
+# t (temps du parcours), v_1 (vitesse au debut), v_2 (vitesse a la fin)
+
+def acceleration_moyenne(t, v_1, v_2):
+    return (v_2-v_1) // t
+
+print(acceleration_moyenne(20, 30, 90))

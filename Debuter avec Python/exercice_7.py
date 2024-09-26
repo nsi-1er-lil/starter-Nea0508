2b1c23143693e21d65db09172e8c39fecab80dc6
Néa Gubert <neag@lyceechurchill.london>
2024-09-26 12:00:35 +0000

    exo.7

---
+++ Debuter avec Python/exercice_7.py
@@ -0,0 +1,10 @@
+# Question 1
+# Les variables sont m (masse), et t (taille). Il s'agit du type float
+
+# Question 2
+# Oui, elle doit renvoyer une reponse du type float
+
+def IMC(m, t):
+    return m//t**2
+
+print(IMC(50, 1.60))

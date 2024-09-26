578b81f4fd11e1e6e1a2fe8f2941b942ab37d8f2
Néa Gubert <neag@lyceechurchill.london>
2024-09-26 11:56:25 +0000

    exo6

---
+++ Debuter avec Python/exercice_6.py
@@ -0,0 +1,17 @@
+# Question 1
+# Aire = r**2*pi
+# volume = (aire)*(h)
+
+# Question 2
+# Les variables utilisees sont h (hauteur) et r (rayon de la base)
+
+# Question 3
+def aire(r):
+    return r**2*3.14
+
+print(aire(5))
+
+def volume(r, h):
+    return (r**2*3.14)*(h)
+
+print(volume(5,6))

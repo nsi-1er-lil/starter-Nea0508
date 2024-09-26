2a0880dafe34e984a25a56d9a9af287989371446
Néa Gubert <neag@lyceechurchill.london>
2024-09-26 11:51:20 +0000

    exo5

--- Debuter avec Python/exercice_5.py
+++ Debuter avec Python/exercice_5.py
@@ -1,6 +1,9 @@
 # Question 1
 # Le perimetre d'un cercle est donne par la formule: 2*r*pi
 
+
 #Question 3
-def perimetre_cercle:
-    return 
+def perimetre_cercle(r):
+    return 2*3.14*r
+
+print(perimetre_cercle(5))

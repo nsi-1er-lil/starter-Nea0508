00ffe24d3c21130fe837e0ca61805a6b1b71d33f
Néa Gubert <neag@lyceechurchill.london>
2024-09-29 21:33:36 +0000

    ex.1

---
+++ Instructions_conditionnelles/exercice_1.py
@@ -0,0 +1,12 @@
+'''
+Question 2:
+L'algorithme renvoie une phrase en fonction de la taille donnee
+
+Exemple 1: 
+taille = 160 
+Resultat: Vous pouvez entrer, ne vomissez pas svp.
+
+Exemple 2: 
+taille = 120
+Resultat: Vous êtes trop petits, allez manger de la soupe.
+'''
